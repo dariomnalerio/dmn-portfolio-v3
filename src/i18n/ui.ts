6d021c33db6988981236c8f0f9ead1542ff7c737
dariomnalerio <dariomnalerio@gmail.com>
2024-05-27 23:06:59 -0300

    Add: i18n and translation implementation

---
+++ src/i18n/ui.ts
@@ -0,0 +1,14 @@
+import en from "../data/locale/en.json"
+import es from "../data/locale/es.json"
+
+export const languages = {
+  en: 'English',
+  es: 'Español',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en,
+  es
+} as const;
